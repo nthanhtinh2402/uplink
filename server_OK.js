0ce6ba1f513a8a26f344913ba82547a646812381
Thanh Tình Nguyễn <79440688+nthanhtinh2402@users.noreply.github.com>
2025-04-12 14:14:30 +0700

    Update server_OK.js

--- server_OK.js
+++ server_OK.js
@@ -200,7 +200,7 @@ app.get('/getlink', (req, res) => {
 });
 
 (async () => {
-    browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     page = await browser.newPage();
     await page.setViewport({ width: 1000, height: 800 });
 
